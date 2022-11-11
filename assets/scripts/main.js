fa76cdfd3171977d8c1aaba2f81a14d94d83646a
pdn002 <pdn002@ucsd.edu>
2022-11-10 23:37:37 -0800

    Added code to all question but not functional

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,14 +45,36 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if (!('serviceWorker' in navigator)){
+    console.error('service worker not supported');
+    return;
+  }
+
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  window.addEventListener('load', async function(event){
+
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    // B3. TODO - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+    try{
+      const registration = await navigator.serviceWorker.register("./sw.js", {
+        scope: "./",
+      });
+
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      if (registration.active){
+        console.log("service worker active");
+      }
+    }
+    // B5. TODO - In the event that the service worker registration fails, console
+    //            log that it has failed.
+    
+    catch(err){
+      console.error(`Registration failed with ${err}`);
+    }
+  });
+
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
